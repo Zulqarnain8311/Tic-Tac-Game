00a86936f2b7d60c05920e54c4952b692394ee0b
Zulqarnain Haider <Zulqarnainhaider8311@gmail.com>
2024-11-12 00:00:00 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import TicTac from "./components/TicTac";
+
+function App() {
+  return (
+    <>
+      <TicTac />
+    </>
+  );
+}
+
+export default App;
